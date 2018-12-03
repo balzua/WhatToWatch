c7b12e839ff090329f55ebdb7e9648e64feaccf4
Brian Alzua <balzua@gmail.com>
2018-12-02 21:47:26 -0600

    added movie search, logs JSON to console

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+const mdb_api_key = 'c3a54b08f36afeb83e13c3643c7c2acd';
 
-
+//A function which accepts a user-provided search term and searches the The Movie Database for people, TV shows, and movies related to that term.
+//Returns a JSON object containing the top 20 search results. 
+function search(searchTerm) {
+    const baseUrl = 'https://api.themoviedb.org/3/search/multi';
+    let params = `api_key=${mdb_api_key}&query=${encodeURIComponent(searchTerm)}`;
+    let requestURL = baseUrl + '?' + params;
+    fetch(requestURL)
+        .then(response => response.json())
+        .then(responseJson => console.log(responseJson));
+    return 'ok'
+}
 
 function submitListener() {
     $('#js-search').on('submit', function (event) {
         event.preventDefault();
-        console.log("Form Submitted");
+        let query = $('#js-search-term').val();
+        let resultsList = search(query);
     });
 }
 
