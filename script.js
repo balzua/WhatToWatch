e67e6cbaa2d8eec250a66fd9c0830976259cb7ab
Brian Alzua <balzua@gmail.com>
2018-12-09 22:07:48 -0600

    YT fix

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function dateConversion(dateJson) {
 //Accepts JSON related to a youtube video and displays it on the page.
 function displayYoutubeVideo(video) {
     $('.right').html(`<iframe title="YouTube Video Player" class="youtube-player" type="text/html" 
-    src="https://www.youtube.com/embed/${video.items[0].id.videoId}&origin=https://balzua.github.io/WhatToWatch/index.html"
+    src="//www.youtube.com/embed/${video.items[0].id.videoId}&origin=https://balzua.github.io/WhatToWatch/"
     width="420" height="315" frameborder="0" allowFullScreen></iframe>`);
     $('.right').removeClass('hidden');
 }
